4f9f8180f615cf2c1b40e957ed509ec476eb4f58
Максим Ефимов <efimov_m@t.top-academy.ru>
2024-03-18 19:09:45 +0300

    Добавьте файлы проекта.

---
+++ CppHomeworks3Project/CppHomeworks3Project.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+
+int main()
+{
+    char symbol;
+    int count;
+    char* str = nullptr;
+    char* strTemp = nullptr;
+
+    while (true)
+    {
+        std::cout << "Input symbol: ";
+        std::cin >> symbol;
+        
+        if (symbol == '~') break;
+
+        std::cout << "Input count: ";
+        std::cin >> count;
+
+        int length = ((str) ? strlen(str) : 0);
+        int sizeNew = count + 1 + length;
+        strTemp = new char[sizeNew];
+
+        for (int i{}; i < length; i++)
+            strTemp[i] = str[i];
+
+        for (int i{}; i < count; i++)
+            strTemp[i + length] = symbol;
+
+        strTemp[length + count] = '\0';
+        if(str)
+            delete[] str;
+        str = strTemp;
+    }
+
+    std::cout << str;
+}
